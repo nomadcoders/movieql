d550e11f22f97edf67e7ff2f366edf1c4078dd8b
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2018-06-20 14:48:47 +0300

    #4 Creating the first Query and Resolver

---
+++ graphql/resolvers.js
@@ -0,0 +1,7 @@
+const resolvers = {
+  Query: {
+    name: () => "nicolas"
+  }
+};
+
+export default resolvers;
--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 import { GraphQLServer } from "graphql-yoga";
+import resolvers from "./graphql/resolvers";
 
-const server = new GraphQLServer({});
+const server = new GraphQLServer({
+  typeDefs: "graphql/schema.graphql",
+  resolvers
+});
 
 server.start(() => console.log("Graphql Server Running"));
