3ead4cfdf6d53a99aceec350fb3598dd93ef40b1
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2018-06-20 15:49:25 +0300

    #9 Creating first Mutation

--- graphql/db.js
+++ graphql/db.js
@@ -1,49 +1,49 @@
-export const people = [
+let movies = [
   {
-    id: "0",
-    name: "Nicolas",
-    age: 18,
-    gender: "female"
+    id: 0,
+    name: "Star Wars - The new one",
+    score: 1
   },
   {
-    id: "1",
-    name: "Jisu",
-    age: 18,
-    gender: "female"
+    id: 1,
+    name: "Avengers - The new one",
+    score: 8
   },
   {
-    id: "2",
-    name: "Japan Guy",
-    age: 18,
-    gender: "male"
+    id: 2,
+    name: "The Godfather I",
+    score: 99
   },
   {
-    id: "3",
-    name: "Daal",
-    age: 18,
-    gender: "male"
-  },
-  {
-    id: "4",
-    name: "JD",
-    age: 18,
-    gender: "male"
-  },
-  {
-    id: "5",
-    name: "moondaddi",
-    age: 18,
-    gender: "male"
-  },
-  {
-    id: "6",
-    name: "flynn",
-    age: 18,
-    gender: "male"
+    id: 3,
+    name: "Logan",
+    score: 2
   }
 ];
 
+export const getMovies = () => movies;
+
 export const getById = id => {
-  const filteredPeople = people.filter(person => person.id === String(id));
-  return filteredPeople[0];
+  const filteredMovies = movies.filter(movie => movie.id === String(id));
+  return filteredMovies[0];
+};
+
+export const deleteMovie = id => {
+  const cleanedMovies = movies.filter(movie => movie.id !== String(id));
+  if (movies.length > cleanedMovies.length) {
+    movies = cleanedMovies;
+    return true;
+  } else {
+    return false;
+  }
+};
+
+export const addMovie = (name, score) => {
+  const newMovie = {
+    id: `${movies.length + 1}`,
+    name,
+    score
+  };
+  movies.push(newMovie);
+  return newMovie;
 };
--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,9 +1,12 @@
-import { people, getById } from "./db";
+import { getMovies, getById, addMovie } from "./db";
 
 const resolvers = {
   Query: {
-    people: () => people,
-    person: (_, { id }) => getById(id)
+    movies: () => getMovies(),
+    movie: (_, { id }) => getById(id)
+  },
+  Mutation: {
+    addMovie: (_, { name, score }) => addMovie(name, score)
   }
 };
 
