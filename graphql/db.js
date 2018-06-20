805835be3f3672bdee40f5f381454a94d13bce2e
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2018-06-20 16:20:58 +0300

    #12 Wrapping a REST API with GraphQL Part Two

--- graphql/db.js
+++ graphql/db.js
@@ -1,8 +1,15 @@
 import fetch from "node-fetch";
-const API_URL = "https://yts.am/api/v2/list_movies.json";
+const API_URL = "https://yts.am/api/v2/list_movies.json?";
 
 export const getMovies = (limit, rating) => {
-  fetch(`${API_URL}`)
+  let REQUEST_URL = API_URL;
+  if (limit > 0) {
+    REQUEST_URL += `limit=${limit}`;
+  }
+  if (rating > 0) {
+    REQUEST_URL += `&minimum_rating=${rating}`;
+  }
+  return fetch(REQUEST_URL)
     .then(res => res.json())
     .then(json => json.data.movies);
 };
--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -2,7 +2,7 @@ import { getMovies } from "./db";
 
 const resolvers = {
   Query: {
-    movies: () => getMovies()
+    movies: (_, { rating, limit }) => getMovies(limit, rating)
   }
 };
 
