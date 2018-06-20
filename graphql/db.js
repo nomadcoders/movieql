66f8d010b09b9b3267dce44c31b788ef27e5e272
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2018-06-20 15:11:41 +0300

    #6 Extending the Schema part Two

---
+++ graphql/db.js
@@ -0,0 +1,49 @@
+export const people = [
+  {
+    id: "0",
+    name: "Nicolas",
+    age: 18,
+    gender: "female"
+  },
+  {
+    id: "1",
+    name: "Jisu",
+    age: 18,
+    gender: "female"
+  },
+  {
+    id: "2",
+    name: "Japan Guy",
+    age: 18,
+    gender: "male"
+  },
+  {
+    id: "3",
+    name: "Daal",
+    age: 18,
+    gender: "male"
+  },
+  {
+    id: "4",
+    name: "JD",
+    age: 18,
+    gender: "male"
+  },
+  {
+    id: "5",
+    name: "moondaddi",
+    age: 18,
+    gender: "male"
+  },
+  {
+    id: "6",
+    name: "flynn",
+    age: 18,
+    gender: "male"
+  }
+];
+
+export const getById = id => {
+  const filteredPeople = people.filter(person => people.id === id);
+  return filteredPeople[0];
+};
--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,12 +1,8 @@
-const nicolas = {
-  name: "Nicolas",
-  age: 18,
-  gender: "female"
-};
+import { people } from "./db";
 
 const resolvers = {
   Query: {
-    person: () => nicolas
+    people: () => people
   }
 };
 
